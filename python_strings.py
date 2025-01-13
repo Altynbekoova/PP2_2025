b5437f9fdf5a3c36a1b9f7075a47fb6f0c2f7aae
Aru <altynbekova.aruzhan065@gmail.com>
2025-01-13 19:29:50 +0500

    I finished:)

---
+++ python_strings.py
@@ -0,0 +1,6 @@
+# Строковые операции
+text = "Hello, Python!"
+print(text.upper())   # Вверхний регистр
+print(text.lower())   # Нижний регистр
+print(text[0:5])      # Срез строки
+print(len(text))      # Длина строки
