2b4bcc369252037a97330c13074fdd2abe5ddaac
Aru <altynbekova.aruzhan065@gmail.com>
2025-01-20 18:33:27 +0500

    lists

---
+++ w2/lists.py
@@ -0,0 +1,16 @@
+# create lists
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+print(fruits[0])  # apple
+
+# add and delete elements
+fruits.append("mango")
+print(fruits)
+
+fruits.remove("mango")
+print(fruits)
+
+
+for fruit in fruits:
+    print(fruit)
