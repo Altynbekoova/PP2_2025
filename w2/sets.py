2286de036ba9fd8a9548fba8a263c105d5a9bb75
Aru <altynbekova.aruzhan065@gmail.com>
2025-01-20 18:37:01 +0500

    sets

---
+++ w2/sets.py
@@ -0,0 +1,10 @@
+# create sets
+unique_numbers = {1, 2, 3, 3, 4}
+print(unique_numbers)  # (1, 2, 3, 4)
+
+# add and delete elements
+unique_numbers.add(5)
+print(unique_numbers)
+
+unique_numbers.remove(2)
+print(unique_numbers)
