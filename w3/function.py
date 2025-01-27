9b5e31172cb74817e73d5747283a61eeb08aeadb
Aru <altynbekova.aruzhan065@gmail.com>
2025-01-27 11:41:22 +0500

    week3

---
+++ w3/function.py
@@ -0,0 +1,19 @@
+
+def greet(name):
+    return f"Hi, {name}!"
+
+print(greet("Aruzhan"))  #Привет, Аружан!
+
+
+
+def add_numbers(a, b):
+    return a + b
+
+print(add_numbers(3, 5))  #8
+
+
+def introduce(name, age=18):
+    return f"my name is {name}, I'm {age} ."
+
+print(introduce("Ару")) 
+print(introduce("Aruzhan", 20)) 
